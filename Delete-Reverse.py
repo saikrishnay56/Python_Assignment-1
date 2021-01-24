ee9a231033c7e20017a8c59a9861cd45582a95ee
saikrishnay56 <77909873+saikrishnay56@users.noreply.github.com>
2021-01-25 04:12:09 +0530

    Add files via upload

---
+++ Delete-Reverse.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+s = "python"
+n = len(s)
+a = s[0:n-2]
+print(a)
+print("\nReverse String")
+print(a[::-1])
---
+++ Replace.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+abc = "I love playing with python"
+x = abc.replace("python", "pythons")
+print(x)
